a716737b7c04ed63e9ea4c5e00e0ee0f5c812942
x0x0b <x0x0b@protonmail.com>
2018-09-06 16:40:12 +0900

    v1.0

--- app.py
+++ app.py
@@ -1,17 +1,52 @@
-from flask import Flask, render_template
+# https: // qiita.com/nagataaaas/items/5c7c9ec4813fea85c40c
+
+import datetime
+
+from flask import Flask, render_template, request
+
+from sqlalchemy import create_engine, Column, String, Integer, DATETIME
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session
 
 app = Flask(__name__)
+engine = create_engine('sqlite:///app.db')
+Base = declarative_base()
 
 
-def getContent():
-    return [{'name': 'Tanaka', 'text': 'こんにちは'}, {'name': 'Kondo', 'text': 'Nice to meet you'}]
+class Content(Base):
+    __tablename__ = 'contents'
+    id = Column(Integer, primary_key=True, unique=True)
+    name = Column(String)
+    content = Column(String)
+    timestamp = Column(DATETIME)
+
+    def __repr__(self):
+        return "Content<{}, {}, {}, {}>".format(self.id, self.name, self.content, self.timestamp)
 
 
-@app.route('/')
-def index():
-    return render_template('index.html', content=getContent())
+Base.metadata.create_all(engine)
+SessionMaker = sessionmaker(bind=engine)
+session = scoped_session(SessionMaker)
 
 
-if __name__ == '__main__':
-    app.debug = True
-    app.run()
+@app.route("/", methods=["GET", "POST"])
+def main_page():
+    cont = session.query(Content).all()
+    if request.method == "GET":
+        return render_template("index.html", cont=cont)
+    elif request.method == "POST":
+        if not request.form["content"]:
+            return render_template("index.html", cont=cont)
+        postname = "ななしさん" if not request.form["name"] else request.form["name"]
+        newpost = Content(
+            name=postname, content=request.form["content"], timestamp=datetime.datetime.now())
+        session.add(newpost)
+        session.commit()
+        cont = session.query(Content).all()
+        return render_template("index.html", cont=cont)
+    else:
+        return render_template("index.html", cont=cont)
+
+
+if __name__ == "__main__":
+    app.run(threaded=True)
